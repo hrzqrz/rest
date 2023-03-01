4c416459f4c7f533b005d551e6f74c196bc0b2a9
safa <hrzqrz@gmail.com>
2023-03-01 16:02:09 +0330

    user registration and error showing was completed

--- rest/accounts/signals.py
+++ rest/accounts/signals.py
@@ -7,7 +7,6 @@ def post_save_create_profile_receiver(sender, instance, created, **kwargs):
     print(created)
     if created:
         UserProfile.objects.create(user=instance)
-        print('user profile is created')
     else:
         try:
             profile = UserProfile.objects.get(user=instance)
@@ -15,11 +14,10 @@ def post_save_create_profile_receiver(sender, instance, created, **kwargs):
         except:
             #create the userprofile if not exist
             UserProfile.objects.create(user=instance)
-            print('profile was not exists but i created 1')
-        print('User updated')
+           
 
 @receiver(pre_save, sender=User)        
 def pre_save_profile_receiver(sender, instance, **kwargs):
-    print(instance.username, 'this user is being saved')
+    pass
         
 #post_save.connect(post_save_create_profile_receiver, sender=User)
