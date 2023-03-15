3dce5be9a140d7fc9cecdb7cd9b3ca73a7713920
safa <hrzqrz@gmail.com>
2023-03-15 17:08:00 +0330

    Dynamic Tax Module

--- rest/marketplace/admin.py
+++ rest/marketplace/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Cart
+from .models import Cart, Tax
 
 # Register your models here.
 
@@ -7,3 +7,9 @@ class CartAdmin(admin.ModelAdmin):
     list_display = ('user', 'fooditem', 'quantity', 'updated_at')
 
 admin.site.register(Cart, CartAdmin)
+
+
+class TaxAdmin(admin.ModelAdmin):
+    list_display = ('tax_type', 'tax_percentage', 'is_active') 
+    
+admin.site.register(Tax, TaxAdmin)
--- rest/resthub/static/js/custome.js
+++ rest/resthub/static/js/custome.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
                     // subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'])
                 }
             }
@@ -76,7 +76,7 @@ $(document).ready(function(){
                     // subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']);
 
                     if(window.location.pathname = '/cart/'){
@@ -118,7 +118,7 @@ $(document).ready(function(){
                     // subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']);
 
                     removeCartItem(0, cart_id);
@@ -149,11 +149,18 @@ $(document).ready(function(){
     // check if the cart is empty
 
     // apply cart amounts
-    function applyCartAmounts(subtotal, tax, grand_total){
+    function applyCartAmounts(subtotal, tax_dict, grand_total){
         if(window.location.pathname == '/cart/'){
             $('#subtotal').html(subtotal)
-            $('#tax').html(tax)
             $('#total').html(grand_total)
+
+            console.log(tax_dict)
+            for(key1 in tax_dict){
+                console.log(tax_dict[key1])
+                for(key2 in tax_dict[key1]){
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+                }
+            }
         }
         
     }
